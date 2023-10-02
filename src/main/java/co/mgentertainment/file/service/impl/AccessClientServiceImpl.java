d8c447c34e368fda716de33bef7842311394b9ca
larry <larryland000@gmail.com>
2023-10-02 19:41:31 +0400

    update

--- src/main/java/co/mgentertainment/file/service/impl/AccessClientServiceImpl.java
+++ src/main/java/co/mgentertainment/file/service/impl/AccessClientServiceImpl.java
@@ -2,14 +2,20 @@ package co.mgentertainment.file.service.impl;
 
 import co.mgentertainment.common.utils.SecurityHelper;
 import co.mgentertainment.file.dal.po.AccessClientDO;
+import co.mgentertainment.file.dal.po.AccessClientExample;
 import co.mgentertainment.file.dal.repository.AccessClientRepository;
 import co.mgentertainment.file.service.AccessClientService;
 import co.mgentertainment.file.service.config.MgfsProperties;
+import co.mgentertainment.file.service.converter.FileObjectMapper;
+import co.mgentertainment.file.service.dto.AccessClientDTO;
 import co.mgentertainment.file.service.dto.ApplyAppAccessDTO;
+import com.google.common.collect.Lists;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -40,5 +46,16 @@ public class AccessClientServiceImpl implements AccessClientService {
     @Override
     public Boolean disableAccess(String appCode) {
         return accessClientRepository.disableAccessClient(appCode);
+    }
+
+    @Override
+    public List<AccessClientDTO> getAllClients() {
+        AccessClientExample example = new AccessClientExample();
+        example.createCriteria().andDisabledEqualTo((byte) 0);
+        List<AccessClientDO> accessClientDOList = accessClientRepository.getAccessClientsByExample(example);
+        if (CollectionUtils.isEmpty(accessClientDOList)) {
+            return Lists.newArrayList();
+        }
+        return FileObjectMapper.INSTANCE.toAccessClientDTOList(accessClientDOList);
     }
 }
