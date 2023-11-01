e6bed56d1f06d3f97af09782ad24a39d35420eb9
larry <larryland000@gmail.com>
2023-11-01 17:18:26 +0400

    update

--- src/main/java/co/mgentertainment/file/service/event/listener/InnerUploadEventListener.java
+++ src/main/java/co/mgentertainment/file/service/event/listener/InnerUploadEventListener.java
@@ -2,6 +2,7 @@ package co.mgentertainment.file.service.event.listener;
 
 import co.mgentertainment.common.devent.DistributedEventCallback;
 import co.mgentertainment.common.devent.annonation.DistributedEventListener;
+import co.mgentertainment.common.utils.cache.CacheStore;
 import co.mgentertainment.file.service.FileService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -22,8 +23,16 @@ public class InnerUploadEventListener implements DistributedEventCallback {
 
     private final FileService fileService;
 
+    private CacheStore localCache = new CacheStore.Builder().setCapacity(10).setExpireSec(3600).build();
+
     @Override
     public void onComplete(String dirPath) {
+        Object obj = localCache.get(dirPath);
+        if ("locker".equals(obj)) {
+            return;
+        }
+        localCache.put(dirPath, "locker");
         fileService.startInnerUploads(new File(dirPath));
+        localCache.remove(dirPath);
     }
 }
